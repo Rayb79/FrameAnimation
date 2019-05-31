087bf6b258f5368948eb62c6ea81ef335c4cf969
Ramaun Bowie <ramaunb@hotmail.com>
2019-05-31 16:23:41 -0400

    Frame Animation

---
+++ app/src/main/java/com/gohool/frameanimation/frameanimation/MainActivity.java
@@ -0,0 +1,44 @@
+package com.gohool.frameanimation.frameanimation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnimationDrawable batAnimation;
+    private ImageView batImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batImage = (ImageView) findViewById(R.id.batID);
+        batImage.setBackgroundResource(R.drawable.bat_anim);
+        batAnimation = (AnimationDrawable) batImage.getBackground();
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        batAnimation.start();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //stop the animation
+                batAnimation.stop();
+
+            }
+        },  5000);  //5 seconds
+
+        return super.onTouchEvent(event);
+    }
+}
